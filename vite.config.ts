e3fb7e41afce065333caa9ffa08b53166ebc7996
Kille <sushanthkille@gmail.com>
2024-10-05 14:56:17 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ["three"],
+          utils: ["lodash.debounce"],
+          framer: ["framer-motion"],
+        },
+      },
+    },
+  },
+});
